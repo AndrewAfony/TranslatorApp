a31c8935ab99b663eb6d4a778ba30ee0f80921b5
Andrew <andrew.afony@yandex.ru>
2022-02-22 11:14:32 +0300

    add view binding to the fragment

--- app/src/main/java/com/myapp/translatorapp/presentation/TranslatorFragment.kt
+++ app/src/main/java/com/myapp/translatorapp/presentation/TranslatorFragment.kt
@@ -6,15 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.myapp.translatorapp.R
+import com.myapp.translatorapp.databinding.FragmentTranslatorBinding
 
 class TranslatorFragment : Fragment() {
+
+    private var _binding: FragmentTranslatorBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_translator, container, false)
+    ): View {
+        _binding = FragmentTranslatorBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
