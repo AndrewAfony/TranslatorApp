d154f32d2cb09f481891f566d758287ebab65ec8
Andrew <andrew.afony@yandex.ru>
2022-02-22 11:46:54 +0300

    add open keyboard after clicking card

--- app/src/main/java/com/myapp/translatorapp/presentation/TranslatorFragment.kt
+++ app/src/main/java/com/myapp/translatorapp/presentation/TranslatorFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.widget.addTextChangedListener
@@ -29,6 +31,10 @@ class TranslatorFragment : Fragment() {
             binding.responseText.text = it.toString()
         }
 
+        binding.enterTextCard.setOnClickListener {
+            openKeyboard(binding.textFromTranslate)
+        }
+
         binding.exitTextCard.setOnClickListener {
             copyToClipboard(binding.responseText.text.toString())
             Toast.makeText(activity, "Copied translation", Toast.LENGTH_SHORT).show()
@@ -37,7 +43,14 @@ class TranslatorFragment : Fragment() {
         return binding.root
     }
 
-    fun copyToClipboard(text: String) {
+    private fun openKeyboard(editText: EditText) {
+        editText.requestFocus()
+
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
+    }
+
+    private fun copyToClipboard(text: String) {
         val clipboard = activity?.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         val clip: ClipData = ClipData.newPlainText("response", text)
         clipboard.setPrimaryClip(clip)
